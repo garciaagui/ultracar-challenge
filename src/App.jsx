a13f59d2776a321c1ec72bcca46dea7508a5e5c1
Guilherme Garcia <garciaguig@gmail.com>
2023-04-17 11:45:50 -0300

    chore: Set up Context and Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import AppProvider from './context/AppProvider';
 
 export function App() {
   return (
-    <div>
+    <AppProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </div>
+    </AppProvider>
   );
 }
 
