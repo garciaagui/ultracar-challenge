aac509b476d6b73da966e54a2f2eb65d70367848
Guilherme Garcia <garciaguig@gmail.com>
2023-04-17 17:44:38 -0300

    feat: Implements `usePersistState` hook

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 const LOGIN_INITIAL_STATE = { email: '', password: '' };
+const USER_INITIAL_STATE = { email: '', name: '' };
 
 const MIN_PASSWORD_LENGTH = 6;
 
-export { LOGIN_INITIAL_STATE, MIN_PASSWORD_LENGTH };
+export { LOGIN_INITIAL_STATE, USER_INITIAL_STATE, MIN_PASSWORD_LENGTH };
