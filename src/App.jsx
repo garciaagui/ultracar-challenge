03d8c46c2fa6a84fed95b19f786c094c130afcae
Guilherme Garcia <garciaguig@gmail.com>
2023-04-18 17:30:32 -0300

    :fire: Remove NotFound page[

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import NotFound from './pages/NotFound';
 import Orders from './pages/Orders';
 import OrderEntry from './pages/OrderEntry';
 import AppProvider from './context/AppProvider';
@@ -12,7 +11,6 @@ export function App() {
         <Route path="/" element={<Login />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/orders/entry" element={<OrderEntry />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </AppProvider>
   );
