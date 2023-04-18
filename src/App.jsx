315250886546a2f4ecbb119d58305fccf89f9f4e
Guilherme Garcia <garciaguig@gmail.com>
2023-04-18 11:29:40 -0300

    feat: Creates Orders page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import Orders from './pages/Orders';
 import OrderEntry from './pages/OrderEntry';
 import AppProvider from './context/AppProvider';
 
@@ -9,6 +10,7 @@ export function App() {
     <AppProvider>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
         <Route path="/orders/entry" element={<OrderEntry />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
